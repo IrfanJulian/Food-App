c54257493eab0aeee1f56299317e3bac66536845
Irfan-Julian <irfanjulian23@gmail.com>
2023-03-07 10:36:41 +0700

    navbar

--- Components/modules/navbar/Navbar.jsx
+++ Components/modules/navbar/Navbar.jsx
@@ -58,8 +58,8 @@ const Navbar = () => {
               <div className="w-11/12">
                 <Image src={logo} width={35} height={35} alt='logo' onClick={()=>router.push('/')} className='' />
               </div>
-              <div className="w-1/12 h-max my-auto">
-                  <button onClick={()=>show === false ? setShow(true) : setShow(false)} className='ml-auto'>
+              <div className="w-1/12 grid">
+                  <button onClick={()=>show === false ? setShow(true) : setShow(false)} className='ml-auto my-auto'>
                     { show === false ?
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-white">
                           <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
